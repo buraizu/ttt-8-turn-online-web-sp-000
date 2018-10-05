7ef5d18d10485bf5c6142913c10902836fe96cdf
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-05 02:09:01 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,13 +14,13 @@ def position_taken?(board, index)
   end
 end
 
-# def valid_move?(board, index)
-#   if index.between?(0, 8) && position_taken?(board, index) == false
-#     return true
-#   else
-#     return false
-#   end
-# end 
+def valid_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index) == false
+    return true
+  else
+    return false
+  end
+end
 
 def input_to_index(string)
   input = string.to_i - 1
