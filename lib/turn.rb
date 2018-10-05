5a56424bbeb9881fee56a59bf3aa3298a90c03b3
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-05 02:07:40 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,9 @@
 def display_board(board)
-  puts board
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def position_taken?(board, index)
@@ -10,10 +14,18 @@ def position_taken?(board, index)
   end
 end
 
-def valid_move?(board, index)
-  if index.between?(0, 8) && position_taken?(board, index) == false
-    return true
-  else
-    return false
-  end
-end 
+# def valid_move?(board, index)
+#   if index.between?(0, 8) && position_taken?(board, index) == false
+#     return true
+#   else
+#     return false
+#   end
+# end 
+
+def input_to_index(string)
+  input = string.to_i - 1
+end
+
+def move(board, index, character="X")
+  board[index] = character
+end
