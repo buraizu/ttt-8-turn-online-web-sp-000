fcb7ac9364321bcc87d1ae5bc37d8bfc93f0c20f
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-05 01:59:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,11 @@
 def display_board(board)
   puts board
 end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
